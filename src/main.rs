a8f24e2b21fbf2c2a364f18788c598639621a649
starkfire <plpantoja49@gmail.com>
2023-07-29 14:05:01 +0800

    initial shell

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use std::env;
+use std::io;
+use std::io::Write;
+use std::path::PathBuf;
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let prompt_string = create_prompt();
+        print!("{}", prompt_string);
+
+        io::stdout().flush().unwrap();
+
+        let mut input_cmd = String::new();
+        io::stdin().read_line(&mut input_cmd)
+            .expect("failed to read stdin");
+
+        let command = input_cmd.trim();
+
+        match command {
+            "exit" => return,
+            command => {
+                let output = Command::new(command).spawn();
+
+                match output {
+                    Ok(mut output) => {
+                        let _ = output.wait();
+                    },
+                    Err(e) => eprintln!("{}", e)
+                }
+            }
+        }
+    }
+}
+
+fn create_prompt() -> String {
+    let cwd = getcwd().unwrap();
+
+    return format!("{}>", cwd.display());
+}
+
+fn getcwd() -> std::io::Result<PathBuf> {
+    env::current_dir()
 }
