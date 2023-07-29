4ba1ddf89bbce34517675086909dd4ed3c8f77ac
starkfire <plpantoja49@gmail.com>
2023-07-30 06:53:28 +0800

    feat: support chaining operators && and ||

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,62 @@
 use std::env;
 use std::io;
 use std::io::Write;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use std::process::{Command, Stdio};
 
-struct CommandTokens<T> {
-    name: String,
-    arguments: Vec<T>
+struct CommandChain<T> {
+    commands: Vec<T>,
+    operators: Vec<T>,
+    arguments: Vec<Vec<T>>
 }
 
 fn main() {
     loop {
-        let prompt_string = create_prompt();
-        print!("{}", prompt_string);
-
+        print!("> ");
         io::stdout().flush().unwrap();
 
         let mut input_cmd = String::new();
-        io::stdin().read_line(&mut input_cmd)
+        io::stdin()
+            .read_line(&mut input_cmd)
             .expect("failed to read stdin");
 
-        let commands = get_commands(&input_cmd);
+        let cmd_tokens = get_command_tokens(&input_cmd);
+        let cmd_chain = parse_cmd_tokens(cmd_tokens);
+        
+        for (idx, cmd) in cmd_chain.commands.iter().enumerate() {
+            let args = &cmd_chain.arguments[idx];
 
-        for command in commands.iter() {
-            let tokens = get_command_tokens(command);
-            let command_name = &*tokens.name;
-
-            match command_name {
+            match *cmd {
                 "exit" => return,
                 "cd" => {
-                    let root = Path::new(tokens.arguments[0]);
+                    let root = Path::new(args[0]);
                     match env::set_current_dir(&root) {
-                        Err(e) => eprintln!("{}", e),
+                        Err(e) => { eprintln!("{}", e) },
                         _ => ()
                     }
                 },
-                command_name => {
-                    let output = Command::new(command_name)
-                        .args(tokens.arguments)
+                cmd => {
+                    let output = Command::new(cmd)
+                        .args(args)
                         .stdout(Stdio::inherit())
                         .spawn();
 
                     match output {
                         Ok(mut output) => {
-                            let _ = output.wait();
+                            let extstatus = output.wait();
+
+                            if let Some(op) = cmd_chain.operators.get(idx) {
+                                if let (false, true) = (extstatus.is_ok(), *op == "&&") { 
+                                    break; 
+                                }
+                            }
+
+                            continue;
                         },
-                        Err(e) => eprintln!("{}", e)
+                        Err(e) => {
+                            eprintln!("{}", e);
+                            continue;
+                        }
                     }
                 }
             }
@@ -53,35 +64,52 @@ fn main() {
     }
 }
 
-fn get_command_tokens(input_cmd: &str) -> CommandTokens<&str> {
-    let tokens: Vec<&str> = input_cmd.trim().split_whitespace().collect();
-    let mut arguments: Vec<&str> = Vec::with_capacity(tokens.len() - 1);
-    arguments.append(&mut tokens[1..].to_vec());
+fn parse_cmd_tokens(tokens: Vec<&str>) -> CommandChain<&str> {
+    let chain_ops: [&str; 2] = ["&&", "||"];
+    
+    let mut commands: Vec<&str> = Vec::new();
+    let mut operators: Vec<&str> = Vec::new();
+    let mut arguments: Vec<Vec<&str>> = vec![vec![]];
+    
+    let mut token_iter = tokens.iter();
 
-    CommandTokens {
-        name: tokens[0].to_string(),
-        arguments
+    let mut prev_token: &str = "";
+
+    while let Some(token) = token_iter.next() { 
+        if commands.len() == 0 {
+            commands.push(token);
+            continue;
+        }
+
+        if chain_ops.contains(token) {
+            operators.push(token);
+            prev_token = token;
+            continue;
+        }
+
+        if chain_ops.contains(&prev_token) {
+            commands.push(token);
+            arguments.push(vec![]);
+        } else {
+            arguments[commands.len() - 1].push(token);
+        }
+
+        prev_token = token;
+    };
+
+    CommandChain {
+        commands,
+        arguments,
+        operators
     }
 }
 
-fn get_commands(input_cmd: &str) -> Vec<&str>{
-    let mut commands: Vec<&str> = input_cmd.trim().split(";").collect();
+fn get_command_tokens(input_cmd: &str) -> Vec<&str> {
+    let mut commands: Vec<&str> = input_cmd.trim().split_whitespace().collect();
 
     for command in commands.iter_mut() {
         *command = command.trim();
     }
 
-    println!("{:?}", &commands);
-
     return commands;
-}
-
-fn create_prompt() -> String {
-    let cwd = getcwd().unwrap();
-
-    return format!("{}>", cwd.display());
-}
-
-fn getcwd() -> std::io::Result<PathBuf> {
-    env::current_dir()
 }
